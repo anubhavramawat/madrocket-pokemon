5974c4a1cada17c2879b345870f2569b1136e48b
Anubhav Ramawat <ramawatanubhav2002@gmail.com>
2025-05-01 17:18:17 +0530

    test changes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,7 +2,7 @@ function PokemonCard({ pokemon }) {
     return (
       <div className="pokemon-card">
         <img src={pokemon.image} alt={pokemon.name} />
-        <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
+        <h3>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} </h3>
         <p>#{pokemon.id}</p>
         <div className="types">
           {pokemon.types.map((type) => (
